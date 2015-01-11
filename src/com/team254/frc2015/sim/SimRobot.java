d54203ec244517a7f1bc72ac252e89cfc2fdcd1d
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2015-01-10 21:09:57 -0800

    First commit

---
+++ src/com/team254/frc2015/sim/SimRobot.java
@@ -0,0 +1,44 @@
+package com.team254.frc2015.sim;
+
+import com.team254.fakewpilib.SimRobotBase;
+
+import edu.wpi.first.wpilibj.EncoderSetter;
+import edu.wpi.first.wpilibj.PWMObserver;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class SimRobot extends SimRobotBase {
+
+	EncoderSetter es = new EncoderSetter(0,1);
+	PWMObserver p1 = new PWMObserver(0);
+	
+  @Override
+  public void initSimRobot() {
+    System.out.println("sim robot init lol");
+    
+  }
+
+  @Override
+  public void startRobotSim() {
+	  System.out.println("starting robot sim!");
+	  double currentPosition = 0;
+	  double speedOfActuator = 4.0; // Inches per second
+	  
+	  double scaleFactorToEncoder = 1250;
+	  while (true) {
+		  
+		  
+		  
+		  currentPosition += ((p1.get() * speedOfActuator) / 100.0);
+		  
+		  int newEnc = (int)(currentPosition * scaleFactorToEncoder);
+		  
+		  System.out.println(p1.get() + " | " + currentPosition + " | " + newEnc);
+		  
+		  es.set(newEnc);
+		  
+		  Timer.delay(.01);
+	  }
+  }
+
+}
