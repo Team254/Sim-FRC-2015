2cd46cfab26a361bc115dd9602a7732276e4c94a
Jared Russell <jared@team341.com>
2015-01-17 16:34:56 -0800

    Add DCMotor simulation and unit test.

---
+++ src/com/team254/frc2015/sim/DCMotor.java
@@ -0,0 +1,96 @@
+package com.team254.frc2015.sim;
+
+public class DCMotor {
+	// Motor constants
+	protected final double m_kt;
+	protected final double m_kv;
+	protected final double m_resistance;
+	
+	// Current motor state
+	protected double m_position;
+	protected double m_velocity;
+	protected double m_current;
+	
+	// Convenience methods for constructing common motors.
+	/**
+	 * Make a model of one or more RS775s (18V version).
+	 * @param num_motors The number of motors in this transmission.
+	 * @param gear_reduction The reduction of the transmission.
+	 * @param efficiency The efficiency of the transmission.
+	 * @return A DCMotor representing the RS775 transmission.
+	 */
+	static DCMotor makeRS775Transmission(int num_motors, double gear_reduction, double efficiency) {
+		// TODO(jared)
+		final double KT = 0.009;  // 9 mNm / A
+		final double KV = 1083.0 * (Math.PI * 2.0) / 60.0;  // 1083 rpm/V in rad/sec/V
+		final double RESISTANCE = (18.0 / 130.0);  // Rated for 130A stall @ 18V
+		return new DCMotor(num_motors * gear_reduction * efficiency * KT,
+				KV / gear_reduction,
+				RESISTANCE / num_motors);
+	}
+	
+	/**
+	 * Simulate a simple DC motor.
+	 * 
+	 * @param kt Torque constant (N*m / amp)
+	 * @param kv Voltage constant (rad/sec / V)
+	 * @param resistance (ohms)
+	 */
+	public DCMotor(double kt, double kv, double resistance) {
+		m_kt = kt;
+		m_kv = kv;
+		m_resistance = resistance;
+		
+		reset(0, 0, 0);
+	}
+	
+	/**
+	 * Reset the motor to a specified state.
+	 * 
+	 * @param position
+	 * @param velocity
+	 * @param current
+	 */
+	public void reset(double position, double velocity, double current) {
+		m_position = position;
+		m_velocity = velocity;
+		m_current = current;
+	}
+	
+	/**
+	 * Simulate applying a given voltage and load for a specified period of time.
+	 * 
+	 * @param applied_voltage Voltage applied to the motor (V)
+	 * @param load Load applied to the motor (kg*m^2)
+	 * @param timestep How long the input is applied (s)
+	 */
+	public void step(double applied_voltage, double load, double timestep) {
+		/*
+		 * Using the 971-style first order system model.
+		 * V = I * R + Kv * w
+         * torque = Kt * I
+         *
+         * V = torque / Kt * R + Kv * w
+         * torque = J * dw/dt
+         *
+         * dw/dt = (V - Kv * w) * Kt / (R * J)
+		 */
+		double acceleration = (applied_voltage - m_velocity / m_kv) * m_kt /
+				(m_resistance * load);
+		m_velocity += acceleration * timestep;
+		m_position += m_velocity * timestep + .5 * acceleration * timestep * timestep;
+		m_current = load * acceleration * Math.signum(applied_voltage) / m_kt;
+	}
+	
+	public double getPosition() {
+		return m_position;
+	}
+	
+	public double getVelocity() {
+		return m_velocity;
+	}
+	
+	public double getCurrent() {
+		return m_current;
+	}
+}
---
+++ src/com/team254/frc2015/sim/TestDCMotor.java
@@ -0,0 +1,142 @@
+package com.team254.frc2015.sim;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDCMotor {
+	private final double EPS = 1E-9;
+
+	@Test
+	public void testGettersSetters() {
+		// Make a new motor.
+		DCMotor motor = new DCMotor(2.0, 10.0, 10.0);
+		
+		// Check initial conditions.
+		assertEquals(motor.getPosition(), 0.0, EPS);
+		assertEquals(motor.getVelocity(), 0.0, EPS);
+		assertEquals(motor.getCurrent(), 0.0, EPS);
+		
+		// Reset initial conditions.
+		motor.reset(1.0, 2.0, 3.0);
+		assertEquals(motor.getPosition(), 1.0, EPS);
+		assertEquals(motor.getVelocity(), 2.0, EPS);
+		assertEquals(motor.getCurrent(), 3.0, EPS);
+	}
+	
+	@Test
+	public void testRS775() {
+		DCMotor rs775 = DCMotor.makeRS775Transmission(1, 10.0, 1.0);
+		
+		// Apply a positive voltage and small load.
+		System.out.println("Voltage=6V, Load=.01 kg*m^2");
+		for (int i = 0; i < 1000; ++i) {
+			rs775.step(6.0, 0.01, 0.01);
+			
+			if (i % 100 == 0) {
+				System.out.print("Time: " + 0.01*i);
+				System.out.print(", Position: " + rs775.getPosition());
+				System.out.print(", Velocity: " + rs775.getVelocity());
+				System.out.println(", Current: " + rs775.getCurrent());
+			}
+		}
+		// We expect negligible final current, and a final velocity of ~68.04 rad/sec.
+		assertEquals(rs775.getCurrent(), 0.0, 1E-3);
+		assertEquals(rs775.getVelocity(), 68.04, 1E-2);
+	
+		// Apply a larger voltage.
+		System.out.println("Voltage=12V, Load=.01 kg*m^2");
+		rs775.reset(0, 0, 0);
+		for (int i = 0; i < 1000; ++i) {
+			rs775.step(12.0, 0.01, 0.01);
+			
+			if (i % 100 == 0) {
+				System.out.print("Time: " + 0.01*i);
+				System.out.print(", Position: " + rs775.getPosition());
+				System.out.print(", Velocity: " + rs775.getVelocity());
+				System.out.println(", Current: " + rs775.getCurrent());
+			}
+		}
+		// We expect negligible final current, and a final velocity of ~2 * 68.04 rad/sec.
+		assertEquals(rs775.getCurrent(), 0.0, 1E-3);
+		assertEquals(rs775.getVelocity(), 68.04*2, 1E-1);
+		
+		// Apply a larger load.
+		System.out.println("Voltage=12V, Load=1.0 kg*m^2");
+		rs775.reset(0, 0, 0);
+		double start_current = 0;
+		for (int i = 0; i < 1000; ++i) {
+			rs775.step(12.0, 1.0, 0.01);
+			if (i == 0) {
+				start_current = rs775.getCurrent();
+			}
+			
+			if (i % 100 == 0) {
+				System.out.print("Time: " + 0.01*i);
+				System.out.print(", Position: " + rs775.getPosition());
+				System.out.print(", Velocity: " + rs775.getVelocity());
+				System.out.println(", Current: " + rs775.getCurrent());
+			}
+		}
+		double final_position = rs775.getPosition();
+		double final_velocity = rs775.getVelocity();
+		double final_current = rs775.getCurrent();
+		
+		// Go in reverse.
+		System.out.println("Voltage=-12V, Load=1.0 kg*m^2");
+		rs775.reset(0, 0, 0);
+		for (int i = 0; i < 1000; ++i) {
+			rs775.step(-12.0, 1.0, 0.01);
+			
+			if (i % 100 == 0) {
+				System.out.print("Time: " + 0.01*i);
+				System.out.print(", Position: " + rs775.getPosition());
+				System.out.print(", Velocity: " + rs775.getVelocity());
+				System.out.println(", Current: " + rs775.getCurrent());
+			}
+		}
+		assertEquals(rs775.getCurrent(), final_current, EPS);
+		
+		
+		// Add a second 775.
+		rs775 = DCMotor.makeRS775Transmission(2, 10.0, 1.0);
+		System.out.println("(2 motors) Voltage=12V, Load=1.0 kg*m^2");
+		for (int i = 0; i < 1000; ++i) {
+			rs775.step(12.0, 1.0, 0.01);
+			if (i == 0) {
+				// We expect to draw twice the starting current.
+				assertEquals(rs775.getCurrent(), 2.0 * start_current, EPS);
+			}
+			
+			if (i % 100 == 0) {
+				System.out.print("Time: " + 0.01*i);
+				System.out.print(", Position: " + rs775.getPosition());
+				System.out.print(", Velocity: " + rs775.getVelocity());
+				System.out.println(", Current: " + rs775.getCurrent());
+			}
+		}
+		// We expect the two motor version to move faster than the single motor version.
+		assert(rs775.getVelocity() - EPS > final_velocity);
+		assert(rs775.getPosition() - EPS > final_position);
+		final_position = rs775.getPosition();
+		final_velocity = rs775.getVelocity();
+
+		// Make it less efficient.
+		rs775 = DCMotor.makeRS775Transmission(2, 10.0, .8);
+		System.out.println("(2 motors, 80% efficient) Voltage=12V, Load=1.0 kg*m^2");
+		for (int i = 0; i < 1000; ++i) {
+			rs775.step(12.0, 1.0, 0.01);
+			
+			if (i % 100 == 0) {
+				System.out.print("Time: " + 0.01*i);
+				System.out.print(", Position: " + rs775.getPosition());
+				System.out.print(", Velocity: " + rs775.getVelocity());
+				System.out.println(", Current: " + rs775.getCurrent());
+			}
+		}
+		// We expect the less efficient version to be slower.
+		assert(rs775.getVelocity() + EPS < final_velocity);
+		assert(rs775.getPosition() + EPS < final_position);
+	}
+
+}
