81a48c2f240b14fea979386207ce14ebe867d5ef
Jared Russell <jared@team341.com>
2015-02-05 20:59:04 -0800

    Add constants

--- src/com/team254/frc2015/sim/ServoMechanism.java
+++ src/com/team254/frc2015/sim/ServoMechanism.java
@@ -6,98 +6,120 @@ import edu.wpi.first.wpilibj.PWMObserver;
 
 /**
  * Simulate a closed-loop position controlled mechanism.
+ * 
  * @author jared
  */
 public class ServoMechanism {
-	protected PWMObserver m_input;
-	protected EncoderSetter m_output;
-	protected double m_encoder_distance_per_tick;
-	protected DCMotor m_model;
-	protected int m_pdp_channel;
-	protected double m_load;
-	protected Limits m_limits;
-	
-	public static class Limits {
-		public Limits(double min, double max) {
-			min_position = min;
-			max_position = max;
-		}
-		public double min_position = -1E9;
-		public double max_position = 1E9;
-	}
-	
-	/**
-	 * Create a new ServoMechanism.
-	 * 
-	 * @param input The source of the PWMs controlling the mechanism.
-	 * @param output The position and rate of the mechanism will be written to this object.
-	 * @param pdp_channel The channel # of the PDP slot for the motor.
-	 * @param encoder_distance_per_tick How to convert between encoder units and real world distance.
-	 * @param model The DCMotor representing the system dynamics.
-	 * @param load The applied load to the mechanism, in kg*m^2
-	 * @param limits Minimum and maximum position limits for the mechanism.
-	 */
-	public ServoMechanism(PWMObserver input,
-			EncoderSetter output,
-			int pdp_channel,
-			double encoder_distance_per_tick,
-			DCMotor model,
-			double load,
-			Limits limits) {
-		m_input = input;
-		m_output = output;
-		m_pdp_channel = pdp_channel;
-		m_encoder_distance_per_tick = encoder_distance_per_tick;
-		m_model = model;
-		m_load = load;
-		m_limits = limits;
-	}
-	
-	/**
-	 * Set the load currently being moved by the mechanism.
-	 * @param load The current mechanism load (in kg*m^2)
-	 */
-	public void setLoad(double load) {
-		m_load = load;
-	}
-	
-	/**
-	 * Reset the position (and encoder value).
-	 * @param position The new position of the mechanism.
-	 */
-	public void reset(double position) {
-		m_model.reset(position, 0.0, 0.0);
-		m_output.set(0);
-		PDP.getInstance().setCurrent(m_pdp_channel, 0);
-	}
-	
-	/**
-	 * Simulate the mechanism's motion over a short time horizon.
-	 * @param battery_voltage The current battery voltage (V).
-	 * @param acceleration The external acceleration being experienced by the mechanism at
-	 * the motor (ex. due to gravity) (rad/s^2).
-	 * @param timestep The simulation period (s).
-	 */
-	public void step(double battery_voltage, double acceleration, double timestep) {
-		double command = m_input.get() * battery_voltage;
-		m_model.step(command, m_load, acceleration, timestep);
-		m_output.set((int)(m_model.getPosition() / m_encoder_distance_per_tick));
-		PDP.getInstance().setCurrent(m_pdp_channel, m_model.getCurrent());
-	}
-	
-	/**
-	 * Check the lower position limit.
-	 * @return True if the current position is greater than or equal to the minimum.
-	 */
-	public boolean withinLowerLimits() {
-		return m_model.getPosition() >= m_limits.min_position;
-	}
-	
-	/**
-	 * Check the upper position limit.
-	 * @return True if the current position is less than or equal to the maximum.
-	 */
-	public boolean withinUpperLimits() {
-		return m_model.getPosition() <= m_limits.max_position;
-	}
+    protected PWMObserver m_input;
+    protected EncoderSetter m_output;
+    protected double m_encoder_distance_per_tick;
+    protected DCMotor m_model;
+    protected double m_start_position;
+    protected int m_pdp_channel;
+    protected double m_load;
+    protected Limits m_limits;
+
+    public static class Limits {
+        public Limits(double min, double max) {
+            min_position = min;
+            max_position = max;
+        }
+
+        public double min_position = -1E9;
+        public double max_position = 1E9;
+    }
+
+    /**
+     * Create a new ServoMechanism.
+     * 
+     * @param input
+     *            The source of the PWMs controlling the mechanism.
+     * @param output
+     *            The position and rate of the mechanism will be written to this
+     *            object.
+     * @param pdp_channel
+     *            The channel # of the PDP slot for the motor.
+     * @param encoder_distance_per_tick
+     *            How to convert between encoder units and real world distance.
+     * @param model
+     *            The DCMotor representing the system dynamics.
+     * @param load
+     *            The applied load to the mechanism, in kg*m^2
+     * @param limits
+     *            Minimum and maximum position limits for the mechanism.
+     */
+    public ServoMechanism(PWMObserver input, EncoderSetter output,
+            int pdp_channel, double encoder_distance_per_tick, DCMotor model,
+            double load, Limits limits) {
+        m_input = input;
+        m_output = output;
+        m_pdp_channel = pdp_channel;
+        m_encoder_distance_per_tick = encoder_distance_per_tick;
+        m_model = model;
+        m_load = load;
+        m_limits = limits;
+        m_start_position = 0;
+    }
+
+    /**
+     * Set the load currently being moved by the mechanism.
+     * 
+     * @param load
+     *            The current mechanism load (in kg*m^2)
+     */
+    public void setLoad(double load) {
+        m_load = load;
+    }
+
+    /**
+     * Reset the position (and encoder value).
+     * 
+     * @param position
+     *            The new position of the mechanism.
+     */
+    public void reset(double position) {
+        m_model.reset(position, 0.0, 0.0);
+        m_start_position = position;
+        m_output.set(0);
+        PDP.getInstance().setCurrent(m_pdp_channel, 0);
+    }
+
+    /**
+     * Simulate the mechanism's motion over a short time horizon.
+     * 
+     * @param battery_voltage
+     *            The current battery voltage (V).
+     * @param acceleration
+     *            The external acceleration being experienced by the mechanism
+     *            at the motor (ex. due to gravity) (rad/s^2).
+     * @param timestep
+     *            The simulation period (s).
+     */
+    public void step(double battery_voltage, double acceleration,
+            double timestep) {
+        double command = m_input.get() * battery_voltage;
+        m_model.step(command, m_load, acceleration, timestep);
+        m_output.set((int) ((m_model.getPosition() - m_start_position) / m_encoder_distance_per_tick));
+        PDP.getInstance().setCurrent(m_pdp_channel, m_model.getCurrent());
+    }
+
+    /**
+     * Check the lower position limit.
+     * 
+     * @return True if the current position is greater than or equal to the
+     *         minimum.
+     */
+    public boolean withinLowerLimits() {
+        return m_model.getPosition() >= (m_limits.min_position - m_start_position);
+    }
+
+    /**
+     * Check the upper position limit.
+     * 
+     * @return True if the current position is less than or equal to the
+     *         maximum.
+     */
+    public boolean withinUpperLimits() {
+        return m_model.getPosition() <= (m_limits.max_position - m_start_position);
+    }
 }
--- src/com/team254/frc2015/sim/SimRobot.java
+++ src/com/team254/frc2015/sim/SimRobot.java
@@ -3,6 +3,7 @@ package com.team254.frc2015.sim;
 import java.util.TimerTask;
 
 import com.team254.fakewpilib.SimRobotBase;
+import com.team254.frc2015.Constants;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.EncoderSetter;
@@ -10,109 +11,104 @@ import edu.wpi.first.wpilibj.PWMObserver;
 import edu.wpi.first.wpilibj.SolenoidStore;
 
 public class SimRobot extends SimRobotBase {
-	final double METERS_PER_INCH = 1.0 / 254.0;
-	final double KG_PER_LB = 1.0 / 2.2;
-	final double ELEVATOR_RADS_PER_INCH = Math.PI / (2.0 * .564);
-	
-	final double pulley_radius_in = .564;
-	final double bottom_carriage_home_height = 20.0 * ELEVATOR_RADS_PER_INCH;
-	final double top_carriage_home_height = 30.0 * ELEVATOR_RADS_PER_INCH;
+    final double METERS_PER_INCH = 1.0 / 254.0;
+    final double KG_PER_LB = 1.0 / 2.2;
+    final double ELEVATOR_RADS_PER_INCH = Math.PI
+            / (2.0 * Constants.kElevatorPulleyRadiusInches);
 
-	PWMObserver bottom_carriage_observer = new PWMObserver(6);
-	PWMObserver top_carriage_observer = new PWMObserver(1);
-	EncoderSetter bottom_carriage_encoder = new EncoderSetter(4, 5);
-	EncoderSetter top_carriage_encoder = new EncoderSetter(6, 7);
-	double elevator_encoder_distance_per_tick = 2.0 * Math.PI / 360.0;  // 360 CPR
+    PWMObserver bottom_carriage_observer = new PWMObserver(
+            Constants.kBottomCarriageMotor1PWM);
+    PWMObserver top_carriage_observer = new PWMObserver(
+            Constants.kTopCarriageMotor1PWM);
+    EncoderSetter bottom_carriage_encoder = new EncoderSetter(
+            Constants.kBottomCarriageEncoderDIOA,
+            Constants.kBottomCarriageEncoderDIOB);
+    EncoderSetter top_carriage_encoder = new EncoderSetter(
+            Constants.kTopCarriageEncoderDIOA,
+            Constants.kTopCarriageEncoderDIOB);
+    double elevator_rads_per_tick = .25 * 2.0 * Math.PI
+            / Constants.kElevatorEncoderCountsPerRev;  // 4x decoding
 
-	ServoMechanism bottom_carriage = new ServoMechanism(bottom_carriage_observer,
-			bottom_carriage_encoder,
-			6,
-			elevator_encoder_distance_per_tick,
-			DCMotor.makeTransmission(DCMotor.makeRS775(), 2, 10.5, .8),
-			0.0,
-			new ServoMechanism.Limits(0.0, 60.0 * ELEVATOR_RADS_PER_INCH)
-			);
-	ServoMechanism top_carriage = new ServoMechanism(top_carriage_observer,
-			top_carriage_encoder,
-			4,
-			elevator_encoder_distance_per_tick,
-			DCMotor.makeTransmission(DCMotor.makeRS775(), 2, 10.5, .8),
-			0.0,
-			new ServoMechanism.Limits(10.0 * ELEVATOR_RADS_PER_INCH, 70.0 * ELEVATOR_RADS_PER_INCH)
-			);
+    ServoMechanism bottom_carriage = new ServoMechanism(
+            bottom_carriage_observer, bottom_carriage_encoder,
+            Constants.kBottomCarriageMotor1PDP,
+            elevator_rads_per_tick, DCMotor.makeTransmission(
+                    DCMotor.makeRS775(), 2, 10.5, .8), 0.0,
+            new ServoMechanism.Limits(
+                    Constants.kBottomCarriageMinPositionInches
+                            * ELEVATOR_RADS_PER_INCH,
+                    Constants.kBottomCarriageMaxPositionInches
+                            * ELEVATOR_RADS_PER_INCH));
+    ServoMechanism top_carriage = new ServoMechanism(top_carriage_observer,
+            top_carriage_encoder, Constants.kTopCarriageMotor1PDP,
+            elevator_rads_per_tick, DCMotor.makeTransmission(
+                    DCMotor.makeRS775(), 2, 10.5, .8), 0.0,
+            new ServoMechanism.Limits(Constants.kTopCarriageMinPositionInches
+                    * ELEVATOR_RADS_PER_INCH,
+                    Constants.kTopCarriageMaxPositionInches
+                            * ELEVATOR_RADS_PER_INCH));
 
-	@Override
-	public void initSimRobot() {
-		System.out.println("sim robot init lol");
+    @Override
+    public void initSimRobot() {
+        System.out.println("sim robot init lol");
 
-	}
+    }
 
-	@Override
-	public void startRobotSim() {
-		System.out.println("starting robot sim!");
+    @Override
+    public void startRobotSim() {
+        System.out.println("starting robot sim!");
 
-		double updateRate = 500.0; // Hz
-		
-		// Initialize.
-		bottom_carriage.reset(bottom_carriage_home_height);
-		top_carriage.reset(top_carriage_home_height);
+        double updateRate = 500.0; // Hz
 
-		class Run extends TimerTask {
-			long loop_counter;
-			
-			@Override
-			public void run() {
-				if (SolenoidStore.getSolenoid(0).get()) {
-					bottom_carriage.setLoad(10.0 * KG_PER_LB * pulley_radius_in * METERS_PER_INCH);  // 10 lbs
-					bottom_carriage.step(12.0, -9.8 / pulley_radius_in, 1.0 / updateRate);
-				} else {
-					bottom_carriage.setLoad(100000.0);  // brake applied
-					bottom_carriage.step(12.0, 0, 1.0 / updateRate);
-				}
+        // Initialize.
+        bottom_carriage.reset(Constants.kBottomCarriageHomePositionInches
+                * ELEVATOR_RADS_PER_INCH);
+        top_carriage.reset(Constants.kTopCarriageHomePositionInches
+                * ELEVATOR_RADS_PER_INCH);
 
-				if (SolenoidStore.getSolenoid(1).get()) {
-					top_carriage.setLoad(10.0 * KG_PER_LB * pulley_radius_in * METERS_PER_INCH);  // 10 lbs
-					top_carriage.step(12.0, -9.8 / pulley_radius_in, 1.0 / updateRate);
-				} else {
-					top_carriage.setLoad(100000.0);  // brake applied
-					top_carriage.step(12.0, 0.0, 1.0 / updateRate);
-				}
-				loop_counter++;
-				if (loop_counter % 100 == 0) {
-					if (!top_carriage.withinLowerLimits()) {
-						System.err.println("Top carriage hit lower limit!");
-					}
-					if (!top_carriage.withinUpperLimits()) {
-						System.err.println("Top carriage hit upper limit!");
-					}
-					if (!bottom_carriage.withinLowerLimits()) {
-						System.err.println("Bottom carriage hit lower limit!");
-					}
-					if (!bottom_carriage.withinUpperLimits()) {
-						System.err.println("Bottom carriage hit upper limit!");
-					}
-				}
-			}
-		}
-		// Wait a second.
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-		
-		java.util.Timer timer = new java.util.Timer();
-		timer.scheduleAtFixedRate(new Run(), 0, (long)((1.0 / updateRate) * 1000.0));
-		
-		// Start auto mode.
-		DriverStation.getInstance().setDSMode(false, true);
-		while(true) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        class Run extends TimerTask {
+            long loop_counter;
+
+            @Override
+            public void run() {
+                if (SolenoidStore.getSolenoid(0).get()) {
+                    bottom_carriage.setLoad(10.0 * KG_PER_LB
+                            * Constants.kElevatorPulleyRadiusInches
+                            * METERS_PER_INCH); // 10 lbs
+                    bottom_carriage.step(12.0, -9.8
+                            / Constants.kElevatorPulleyRadiusInches,
+                            1.0 / updateRate);
+                }
+
+                if (SolenoidStore.getSolenoid(1).get()) {
+                    top_carriage.setLoad(10.0 * KG_PER_LB
+                            * Constants.kElevatorPulleyRadiusInches
+                            * METERS_PER_INCH); // 10 lbs
+                    top_carriage.step(12.0, -9.8
+                            / Constants.kElevatorPulleyRadiusInches,
+                            1.0 / updateRate);
+                }
+                
+                loop_counter++;
+                if (loop_counter % 100 == 0) {
+                    if (!top_carriage.withinLowerLimits()) {
+                        System.err.println("Top carriage hit lower limit!");
+                    }
+                    if (!top_carriage.withinUpperLimits()) {
+                        System.err.println("Top carriage hit upper limit!");
+                    }
+                    if (!bottom_carriage.withinLowerLimits()) {
+                        System.err.println("Bottom carriage hit lower limit!");
+                    }
+                    if (!bottom_carriage.withinUpperLimits()) {
+                        System.err.println("Bottom carriage hit upper limit!");
+                    }
+                }
+            }
+        }
+        java.util.Timer timer = new java.util.Timer();
+        timer.scheduleAtFixedRate(new Run(), 0,
+                (long) ((1.0 / updateRate) * 1000.0));
+    }
 
 }
