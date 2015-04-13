acbdb7125a4552e59ef1febd7a55a942dc9119da
Jared Russell <jared@team341.com>
2015-04-12 21:21:39 -0700

    update peacock sim

--- src/com/team254/frc2015/sim/DCMotor.java
+++ src/com/team254/frc2015/sim/DCMotor.java
@@ -1,112 +1,147 @@
 package com.team254.frc2015.sim;
 
 /**
- * Simulates a DC motor using a simplified model appropriate for systems with slow time constants.
+ * Simulates a DC motor using a simplified model appropriate for systems with
+ * slow time constants.
+ * 
  * @author jared
  */
 public class DCMotor {
-	// Motor constants
-	protected final double m_kt;
-	protected final double m_kv;
-	protected final double m_resistance;
-	
-	// Current motor state
-	protected double m_position;
-	protected double m_velocity;
-	protected double m_current;
-	
-	// Convenience methods for constructing common motors.
-	/**
-	 * Make a model of one or more RS775s (18V version).
-	 * @param num_motors The number of motors in this transmission.
-	 * @param gear_reduction The reduction of the transmission.
-	 * @param efficiency The efficiency of the transmission.
-	 * @return A DCMotor representing the RS775 transmission.
-	 */
-	static DCMotor makeRS775() {
-		final double KT = 0.009;  // 9 mNm / A
-		final double KV = 1083.0 * (Math.PI * 2.0) / 60.0;  // 1083 rpm/V in rad/sec/V
-		final double RESISTANCE = (18.0 / 130.0);  // Rated for 130A stall @ 18V
-		return new DCMotor(KT, KV, RESISTANCE);
-	}
-	
-	static DCMotor makeRS550() {
-	    final double KT = 0.004862;
-	    final double KV = 1608.0 * (Math.PI * 2.0) / 60.0;
-	    final double RESISTANCE = (12.0 / 85.0);
-	    return new DCMotor(KT, KV, RESISTANCE);
-	}
-	
-	static DCMotor makeTransmission(DCMotor motor, int num_motors, double gear_reduction,
-			double efficiency) {
-		return new DCMotor(num_motors * gear_reduction * efficiency * motor.m_kt,
-				motor.m_kv / gear_reduction,
-				motor.m_resistance / num_motors);
-	}
-	
-	/**
-	 * Simulate a simple DC motor.
-	 * 
-	 * @param kt Torque constant (N*m / amp)
-	 * @param kv Voltage constant (rad/sec / V)
-	 * @param resistance (ohms)
-	 */
-	public DCMotor(double kt, double kv, double resistance) {
-		m_kt = kt;
-		m_kv = kv;
-		m_resistance = resistance;
-		
-		reset(0, 0, 0);
-	}
-	
-	/**
-	 * Reset the motor to a specified state.
-	 * 
-	 * @param position
-	 * @param velocity
-	 * @param current
-	 */
-	public void reset(double position, double velocity, double current) {
-		m_position = position;
-		m_velocity = velocity;
-		m_current = current;
-	}
-	
-	/**
-	 * Simulate applying a given voltage and load for a specified period of time.
-	 * 
-	 * @param applied_voltage Voltage applied to the motor (V)
-	 * @param load Load applied to the motor (kg*m^2)
-	 * @param acceleration The external torque applied (ex. due to gravity) (N*m)
-	 * @param timestep How long the input is applied (s)
-	 */
-	public void step(double applied_voltage, double load, double external_torque, double timestep) {
-		/*
-		 * Using the 971-style first order system model.
-		 * V = I * R + Kv * w
+    // Motor constants
+    protected final double m_kt;
+    protected final double m_kv;
+    protected final double m_resistance;
+    protected final double m_motor_inertia;
+
+    // Current motor state
+    protected double m_position;
+    protected double m_velocity;
+    protected double m_current;
+
+    // Convenience methods for constructing common motors.
+    static DCMotor makeRS775() {
+        final double KT = 0.009; // 9 mNm / A
+        final double KV = 1083.0 * (Math.PI * 2.0) / 60.0; // 1083 rpm/V in
+                                                           // rad/sec/V
+        final double RESISTANCE = (18.0 / 130.0); // Rated for 130A stall @ 18V
+        final double INERTIA = 1.20348237e-5; // 127g cylinder @ 1.084" diameter
+        return new DCMotor(KT, KV, RESISTANCE, INERTIA);
+    }
+
+    static DCMotor makeRS550() {
+        final double KT = 0.004862;
+        final double KV = 1608.0 * (Math.PI * 2.0) / 60.0;
+        final double RESISTANCE = (12.0 / 85.0);
+        final double INERTIA = 0; // TODO(jared): Measure this
+        return new DCMotor(KT, KV, RESISTANCE, INERTIA);
+    }
+
+    /*
+     * Make a transmission.
+     * 
+     * @param motors The motor type attached to the transmission.
+     * @param num_motors The number of motors in this transmission.
+     * @param gear_reduction The reduction of the transmission.
+     * @param efficiency The efficiency of the transmission.
+     * @return A DCMotor representing the combined transmission.
+     */
+    static DCMotor makeTransmission(DCMotor motor, int num_motors,
+            double gear_reduction, double efficiency) {
+        return new DCMotor(num_motors * gear_reduction * efficiency
+                * motor.m_kt, motor.m_kv / gear_reduction, motor.m_resistance
+                / num_motors, motor.m_motor_inertia * num_motors
+                * gear_reduction * gear_reduction);
+    }
+
+    /**
+     * Simulate a simple DC motor.
+     * 
+     * @param kt
+     *            Torque constant (N*m / amp)
+     * @param kv
+     *            Voltage constant (rad/sec / V)
+     * @param resistance
+     *            (ohms)
+     * @param inertia
+     *            (kg*m^2)
+     */
+    public DCMotor(double kt, double kv, double resistance, double inertia) {
+        m_kt = kt;
+        m_kv = kv;
+        m_resistance = resistance;
+        m_motor_inertia = inertia;
+
+        reset(0, 0, 0);
+    }
+
+    /**
+     * Simulate a simple DC motor.
+     * 
+     * @param kt
+     *            Torque constant (N*m / amp)
+     * @param kv
+     *            Voltage constant (rad/sec / V)
+     * @param resistance
+     *            (ohms)
+     */
+    public DCMotor(double kt, double kv, double resistance) {
+        this(kt, kv, resistance, 0);
+    }
+
+    /**
+     * Reset the motor to a specified state.
+     * 
+     * @param position
+     * @param velocity
+     * @param current
+     */
+    public void reset(double position, double velocity, double current) {
+        m_position = position;
+        m_velocity = velocity;
+        m_current = current;
+    }
+
+    /**
+     * Simulate applying a given voltage and load for a specified period of
+     * time.
+     * 
+     * @param applied_voltage
+     *            Voltage applied to the motor (V)
+     * @param load
+     *            Load applied to the motor (kg*m^2)
+     * @param acceleration
+     *            The external torque applied (ex. due to gravity) (N*m)
+     * @param timestep
+     *            How long the input is applied (s)
+     */
+    public void step(double applied_voltage, double load,
+            double external_torque, double timestep) {
+        /*
+         * Using the 971-style first order system model. V = I * R + Kv * w
          * torque = Kt * I
-         *
-         * V = torque / Kt * R + Kv * w
-         * torque = J * dw/dt + external_torque
-         *
+         * 
+         * V = torque / Kt * R + Kv * w torque = J * dw/dt + external_torque
+         * 
          * dw/dt = (V - Kv * w) * Kt / (R * J) - external_torque / J
-		 */
-		double acceleration = (applied_voltage - m_velocity / m_kv) * m_kt /
-				(m_resistance * load) + external_torque / load;
-		m_velocity += acceleration * timestep;
-		m_position += m_velocity * timestep + .5 * acceleration * timestep * timestep;
-		m_current = load * acceleration * Math.signum(applied_voltage) / m_kt;
-	}
-	
-	public double getPosition() {
-		return m_position;
-	}
-	
-	public double getVelocity() {
-		return m_velocity;
-	}
-	
-	public double getCurrent() {
-		return m_current;
-	}
+         */
+        load += m_motor_inertia;
+        double acceleration = (applied_voltage - m_velocity / m_kv) * m_kt
+                / (m_resistance * load) + external_torque / load;
+        m_velocity += acceleration * timestep;
+        m_position += m_velocity * timestep + .5 * acceleration * timestep
+                * timestep;
+        m_current = load * acceleration * Math.signum(applied_voltage) / m_kt;
+    }
+
+    public double getPosition() {
+        return m_position;
+    }
+
+    public double getVelocity() {
+        return m_velocity;
+    }
+
+    public double getCurrent() {
+        return m_current;
+    }
 }
--- test/com/team254/frc2015/sim/CanGrabber.java
+++ test/com/team254/frc2015/sim/CanGrabber.java
@@ -8,34 +8,37 @@ public class CanGrabber {
 
     @Test
     public void test() {
-        DCMotor motor = DCMotor.makeTransmission(DCMotor.makeRS775(), 2, 80.0,
+        // The transmission to test.
+        DCMotor motor = DCMotor.makeTransmission(DCMotor.makeRS775(), 2, 77.0,
                 .8);
+        
+        // INPUTS
+        final double battery_voltage = 12.8;
+        final double arm_com = .82;  // m
+        final double arm_mass = .2;  // kg
+        final double battery_r = 0.018;  // ohms
+        // torque provided constantly over the entire range of motion
+        final double constant_spring_assist_torque = 0.0;  // N*m
+        // torque provided varying with sin of angle (same effect as gravity)
+        // due to over-center.
+        final double over_center_spring_assist_torque = 0.0;  // N*m
+        
         double total_time = 0.0;
-        double arm_com = 1.0;
-        double arm_mass = .3;
-        double battery_r = 0.018;
-        double constant_spring_assist_torque = 0.0; // torque provided through
-                                                     // the whole range of
-                                                     // motion
-        double over_center_spring_assist_torque = 0.0; // torque provided
-                                                         // proportional to sin
-                                                         // of angle (direction
-                                                         // of gravity)
+        double arm_inertia = arm_mass * arm_com * arm_com;
         // lower
         while (motor.getPosition() < Math.PI / 2 && total_time < 1.0) {
             double gravity_torque = 9.8 * arm_com * arm_mass
                     * Math.sin(motor.getPosition());
             double current = motor.getCurrent() * 2; // Two peacocks
             motor.step(
-                    12.0 - current * battery_r,
-                    .4,
+                    battery_voltage - current * battery_r,
+                    arm_inertia,
                     gravity_torque + constant_spring_assist_torque
                             + over_center_spring_assist_torque
                             * Math.sin(motor.getPosition()), 0.0001);
             total_time += .0001;
         }
-        System.out.println("Time: " + total_time + ", Angle: "
-                + motor.getPosition());
+        System.out.println("Time to lower mechanism 90 degrees: " + total_time);
 
         // raise
         total_time = 0.0;
@@ -44,8 +47,8 @@ public class CanGrabber {
                     * Math.sin(motor.getPosition());
             double current = motor.getCurrent() * 2; // Two peacocks
             motor.step(
-                    -12.0 + current * battery_r,
-                    .4,
+                    -battery_voltage + current * battery_r,
+                    arm_inertia,
                     gravity_torque + constant_spring_assist_torque
                             + over_center_spring_assist_torque
                             * Math.sin(motor.getPosition()), 0.0001);
@@ -53,8 +56,8 @@ public class CanGrabber {
         }
         if (total_time >= 1.0) {
             System.out.println("CAN NOT RAISE ARM!");
+        } else {
+            System.out.println("Time to raise mechanism back to start angle: " + total_time);
         }
-        System.out.println("Time: " + total_time + ", Angle: "
-                + motor.getPosition());
     }
 }
